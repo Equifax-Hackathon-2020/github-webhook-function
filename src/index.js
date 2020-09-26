9ac14894d68c47eca948a800d7917719626b0ece
rudiduc <rudiduc@gmail.com>
2020-09-26 12:50:41 +1200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@
  * @param {!express:Response} res HTTP response context.
  */
 const Firestore = require('@google-cloud/firestore');
+const crypto = require('crypto')
+
 const PROJECTID = 'equifax-hackathon-2020';
 const COLLECTION_NAME = 'microservice-data';
 const firestore = new Firestore({
     projectId: PROJECTID,
     timestampsInSnapshots: true,
 });
+const secret = CHANGE_ME;
 
 exports.helloWorld = (req, res) => {
+    verifyPostData(req, res);
     let message = req.query.message || req.body.message || 'No data!';
     
     const created = new Date().getTime();
@@ -27,3 +31,20 @@ exports.helloWorld = (req, res) => {
             console.error(err);
         });
 };
+
+function verifyPostData(req, res, next) {
+  const payload = JSON.stringify(req.body)
+  if (!payload) {
+    return next('Request body empty')
+  }
+
+  const sig = req.get(sigHeaderName) || ''
+  const hmac = crypto.createHmac('sha1', secret)
+  const digest = Buffer.from('sha1=' + hmac.update(payload).digest('hex'), 'utf8')
+  const checksum = Buffer.from(sig, 'utf8')
+  if (checksum.length !== digest.length || !crypto.timingSafeEqual(digest, checksum)) {
+    //return next(`Request body digest (${digest}) did not match ${sigHeaderName} (${checksum})`)
+    return false;
+  }
+  return true;
+}
